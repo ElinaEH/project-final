18189a5d6ce63ab65bde48b1f02af7e5f3f60592
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2025-02-09 17:26:11 +0100

    Configured backend CORS to accept connection from both local and deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,32 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Configure CORS middleware to allow requests from frontend
+// Configure CORS middleware to allow requests from frontend 
+// DELETE ONCE UPDATED CONNECTION WORKS
+// app.use(cors({
+//  origin: "http://localhost:5173", // Frontend development server URL
+//  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//  allowedHeaders: ["Content-Type", "Authorization"],
+//  credentials: true
+// }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://soundseed.netlify.app"
+];
+
 app.use(cors({
- origin: "http://localhost:5173", // Frontend development server URL
- methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
- allowedHeaders: ["Content-Type", "Authorization"],
- credentials: true
+  origin: function(origin, callback) {
+    // Check if the origin is in our allowedOrigins array
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 // Parse JSON request bodies
